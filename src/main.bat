71bbcf7d495d5f00c079d960acca23d5117f543e
matthewlim124 <mattlim124816@gmail.com>
2023-11-24 10:31:55 +0700

    update from main + some fixes

--- src/main.bat
+++ src/main.bat
@@ -1,5 +1,5 @@
 @echo off
-gcc ADT/StackLagu/stack.c ADT/Queue/queue.c ADT/SetWord/set.c ADT/Word/word.c ADT/ListLagu/listlagu.c ADT/MesinKarakter/mesinkarakter.c ADT/MesinKata/mesinkata.c main.c console/src/console.c ADT/Lagu/lagu.c ADT/MapWord/map.c ADT/Playlist/playlist.c ADT/Playlist/listlinier.c -o main
+gcc ADT/StackLagu/stack.c ADT/Queue/queue.c ADT/SetWord/set.c ADT/Word/word.c ADT/ListLagu/listlagu.c ADT/MesinKarakter/mesinkarakter.c ADT/MesinKata/mesinkata.c main.c console/src/console.c ADT/Lagu/lagu.c ADT/MapWord/map.c ADT/Playlist/playlist.c ADT/Playlist/listlinier.c DecTree.c -o main
 
 if errorlevel 1 (
     echo Compilation failed.
